1a02642a105b3a541a8a1a1de3c3b1629a770a3d
Jake Mercer <Jake@admins-MacBook-Pro.local>
2018-12-03 18:30:27 -0800

    desert collider

---
+++ Assets/y_1_collide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class y_1_collide : MonoBehaviour {
+
+    public GameObject yellow;
+    public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit(Collider other){
+        Instantiate(yellow, position, Quaternion.identity);
+    }
+}
